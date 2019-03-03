9f24aa079e7ac17a43a39620395b44e4dfa51d3b
luddite478 <luddite478@gmail.com>
2019-03-03 13:45:10 +0300

    restarting function and docker-compose order of execution

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@
 
 export input_dir='./input_stream_segments'
 export output_dir='./processed_stream_segments'
+export status="idle"
 export height="0"
 export width="0"
 export fps="0"
@@ -16,6 +17,7 @@ export segment_size="5"
 export curr_segment="0"
 export ff_loglvl="quiet"
 export hardware_acceleration=$1
+export time_window="4"
 
 if [[ $hardware_acceleration == "GPU" ]]; then
 export ff_decoder="h264_cuvid"
@@ -32,12 +34,27 @@ fi
 
 ################################################################################
 
+fn_wait_next_n_sec_for_new_file_if_no_file_restart() {
+  while [[ status == "idle" ]]; do
+    last_segment_processed_time=$1
+    now=$(date +%s%3N)
+
+    if [[ $(($now - $last_segment_processed_time)) -mt $time_window ]]; then
+      printf "\n\n LAST SEGMENT RECEIVED MORE THAN 4 SEC AGO, RESTARTING ... \n\n"
+      exec "./start.sh"
+    fi
+    sleep 0.01
+  done
+}
+
+################################################################################
+
 fn_segment() {
-ffmpeg -y -hide_banner -fflags +genpts -loglevel $ff_loglvl -i $input_stream_url \
--c:v copy  \
--codec:a copy -start_number 0 -hls_time $segment_size -hls_wrap 5 -hls_list_size 5 -max_muxing_queue_size 1024 \
--hls_segment_filename "$input_dir/%d.mp4" \
--f hls ".m3u8"
+  ffmpeg -y -hide_banner -fflags +genpts -loglevel $ff_loglvl -i $input_stream_url \
+  -c:v copy  \
+  -codec:a copy -start_number 0 -hls_time $segment_size -hls_wrap 5 -hls_list_size 5 -max_muxing_queue_size 1024 \
+  -hls_segment_filename "$input_dir/%d.mp4" \
+  -f hls ".m3u8"
 }
 
 ################################################################################
@@ -46,6 +63,7 @@ fn_process() {
 inotifywait -q -e close_write -m --format "%f" $input_dir |
 while read -r f; do
   start=$(date +%s%3N)
+  status="processing"
 
   filename="$curr_segment.mp4"
 
@@ -65,11 +83,14 @@ while read -r f; do
 
   ffmpeg -loglevel warning -t $segment_size -i "./video_tmp.mp4" -t $segment_size -i "$input_dir/$filename" -map 0:v:0 -map 1:a:0 -c:v $ff_encoder -qp 1 -y "$output_dir/$curr_segment.mp4"
 
+  curr_segment=$((curr_segment+1))
+  if (( curr_segment > "4" )); then curr_segment="0"; fi
+  status="idle"
+
   end=$(date +%s%3N)
   printf "\n\n fn_process runtime: $((end-start)) \n\n"
 
-  curr_segment=$((curr_segment+1))
-  if (( curr_segment > "4" )); then curr_segment="0"; fi
+  fn_wait_next_n_sec_for_new_file_if_no_file_restart $end
 done
 }
 
